818096dd332b81dcce000060deea3732ea7b9adf
alexander Bous <alexmbous@gmail.com>
2018-02-22 16:45:03 -0600

    pushing it all so far

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 
-"use strict";
+
 const $ = require('jquery');
 
 const myName = "Alex";
@@ -7,5 +7,5 @@ const myName = "Alex";
 const sayHello = (myName) => { return ( `hello ${myName}`)};
 console.log(sayHello(myName));
 
-$('body').css('background-color', 'blue');
+$('body').css('background-color', 'salmon');
 
